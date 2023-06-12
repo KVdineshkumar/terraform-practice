480625c4788b2bb0642fc8b53eb2feabbe4d4a3c
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-12 20:33:51 +0530

    ansible cnofig for frontend

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -14,5 +14,54 @@ output "sample_var" {
 }
 ## Sometime if varaible/any reference with combination of some other string then we want to access those in ${}
 output "sample-ext" {
-  value = "Value od sample - ${var.sample}"
+  value = "Value od sample - ${var.sample} - ${var.sample1}"
+}
+#Varaible data types
+# 1. String
+# 2. Number
+# 3. Boolean
+
+# variable type
+# In Ansible - 1.Plain Key,value 2.List 3.Map/dict
+
+# In Terraform
+# 1. Plain
+# 2. List
+# 3. Map
+
+# plain
+variable "course" {
+  default = "devOps Training"
+}
+# List
+variable "courses" {
+  default = [
+  "DevOps",
+  "AWS",
+  "Python"
+  ]
+}
+#Map
+variable "course_details" {
+  default =  {
+    DevOps = {
+      name  = "DevOps"
+      timing  = "10am"
+      duration  = 90
+    }
+    AWS= {
+      name = "AWS"
+      timing  = "11am"
+      duration  = 30
+    }
+  }
+}
+output "course" {
+  value = var.course
+}
+output "courses" {
+  value = var.courses
+}
+output "course_details" {
+  value = var.course_details
 }
