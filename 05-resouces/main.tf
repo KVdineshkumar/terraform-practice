fd6cadedd0bcbcd7091a2aa14e1f9bb34f509ad1
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-12 22:57:06 +0530

    ansible cnofig for frontend

--- 05-resouces/main.tf
+++ 05-resouces/main.tf
@@ -1,10 +1,16 @@
 
 
 resource "aws_instance" "hey" {
-  ami           = ami-03265a0778a880afb
+  ami           = "sg-09a3d1d154d7ca8e2"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+#data "aws_ami" "example" {
+#  owners      = ["973714476881"]
+ # most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#}
