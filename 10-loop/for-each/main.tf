26a31606f7456e4d8d81087ed3f3706741b57773
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-15 11:34:27 +0530

    hey

--- 10-loop/for-each/main.tf
+++ 10-loop/for-each/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   //count = length(var.instances)
   for_each = var.instances
   ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
+  instance_type = lookup(each.value, "instance_type", "t3.small" )
 
   tags = {
     Name = "hello"
@@ -18,7 +18,7 @@ variable "instances" {
   default = {
     frontend = {
       name = "frontend"
-      instance_type = "t3.nano"
+
     }
     cart = {
       name = "cart"
